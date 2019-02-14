818277fff516ae850b1751e7c8022e88121be138
Daniel <dannykhosh@gmail.com>
2019-02-13 19:36:06 -0500

    dxgr

--- src/main/java/frc/robot/commands/ShiftGear.java
+++ src/main/java/frc/robot/commands/ShiftGear.java
@@ -8,13 +8,16 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
 
-public class ShiftGear extends Command {
+public class ShiftGear extends InstantCommand {
   private Robot _robot;
-  public ShiftGear(Robot robot) {
+  public boolean shifty;
+  public ShiftGear(Robot robot, boolean shift) {
     _robot = robot;
     requires(_robot.driveTrain);
+    shifty = shift;
   }
 
   // Called just before this Command runs the first time
@@ -26,7 +29,8 @@ public class ShiftGear extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-      _robot.driveTrain.setShifterSolenoid(true); //Shifts to High Gear
+      _robot.driveTrain.setShifterSolenoid(shifty);
+     //Shifts to High Gear
     }
   
   // Make this return true when this Command no longer needs to run execute()
@@ -38,12 +42,9 @@ public class ShiftGear extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-    _robot.driveTrain.setShifterSolenoid(false);
-  }
 }
