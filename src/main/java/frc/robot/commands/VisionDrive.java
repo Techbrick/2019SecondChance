671ae057fd2ca4967bddca02b558624f65517ffa
Russell Chiu <chiurj2002@gmail.com>
2019-02-23 12:28:45 -0500

    Fixes made to VisionDrive

--- src/main/java/frc/robot/commands/VisionDrive.java
+++ src/main/java/frc/robot/commands/VisionDrive.java
@@ -16,19 +16,19 @@ import frc.robot.TurnPid;
 public class VisionDrive extends Command {
   private Robot _robot;
   private double targetAngle;
-  private double absoluteAngle;
+  private double absTargetAngle;
   private double difference;
   private double tx;
   private Helpers helper;
   private boolean drive = true;
   private TurnPid turny;
+  private double absCurrentAngle;
   public VisionDrive(Robot robot, int angle) {
     _robot = robot;
     requires(robot.driveTrain);
     targetAngle = angle;
     // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);==
-    
+    // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
@@ -43,36 +43,38 @@ public class VisionDrive extends Command {
   @Override
   protected void execute() {
     //Difference between absolute angle and target angle, then steer to it with difference * 1.5
-        // double steer = m_Controller.getX(Hand.kRight);
-        // double drive = -m_Controller.getY(Hand.kLeft);
-        // boolean auto = m_Controller.getAButton();
-        // steer *= 0.70;
-        // drive *= 0.70;
-        tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
-        absoluteAngle = tx + _robot.navX.getYaw();
-        difference = absoluteAngle - targetAngle;
-        _robot.driveTrain.Update_Limelight_Tracking();
-        turny.SetTargetAngle(Helpers.ConvertYawToHeading(difference*1.5 + targetAngle));
-        if (drive)
-        {
-          if (_robot.driveTrain.m_LimelightHasValidTarget)
-          {
-            double drv = -_robot.driveTrain.m_LimelightDriveCommand;
-            double turn = turny.GetAnglePidOutput(helper.ConvertYawToHeading(tx));
-            SmartDashboard.putNumber("VD drv", drv);
-            SmartDashboard.putNumber("VD Turn", turn);
-            _robot.driveTrain.Move(drv - turn, -(drv + turn));
-          // _robot.driveTrain.Move(-turn, -turn);
-           }
-          else
-          {
-                _robot.driveTrain.ArcadeDrive(0.0,0.0);
-          }
-        }
-        else
-        {
-          _robot.driveTrain.ArcadeDrive(0.0,0.0);
-        }
+    // double steer = m_Controller.getX(Hand.kRight);
+    // double drive = -m_Controller.getY(Hand.kLeft);
+    // boolean auto = m_Controller.getAButton();
+    // steer *= 0.70;
+    // drive *= 0.70;
+    tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    absTargetAngle = Helpers.ConvertYawToHeading(tx + _robot.navX.getYaw());
+    absCurrentAngle = Helpers.ConvertYawToHeading(_robot.navX.getYaw());
+    difference = 1.5 * (absTargetAngle - absCurrentAngle);
+    // difference = absTargetAngle - targetAngle;
+    _robot.driveTrain.Update_Limelight_Tracking();
+    // turny.SetTargetAngle(Helpers.ConvertYawToHeading(difference*1.5 + targetAngle));
+    turny.SetTargetAngle(Helpers.ConvertYawToHeading(absTargetAngle));
+
+    SmartDashboard.putNumber("Target", absTargetAngle);
+    SmartDashboard.putNumber("Current", absCurrentAngle);
+    SmartDashboard.putNumber("Angle Error", difference);
+    if (drive && _robot.driveTrain.m_LimelightHasValidTarget)
+    {
+      double drv = -_robot.driveTrain.m_LimelightDriveCommand;
+      // double turn = _robot.driveTrain.m_LimelightSteerCommand;
+      // double turn = turny.GetAnglePidOutput(helper.ConvertYawToHeading(tx));
+      double turn = turny.GetAnglePidOutput(Helpers.ConvertYawToHeading(absCurrentAngle + difference));
+      SmartDashboard.putNumber("VD drv", drv);
+      SmartDashboard.putNumber("VD Turn", turn);
+      // _robot.driveTrain.Move(drv - turn, -(drv + turn));
+      _robot.driveTrain.Move(drv + turn, turn - drv);
+    }
+    else
+    {
+      _robot.driveTrain.ArcadeDrive(0.0,0.0);
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
